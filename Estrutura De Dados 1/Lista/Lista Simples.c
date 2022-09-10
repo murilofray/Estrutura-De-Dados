4a5c112c2c5d346eecb39a924eb4c78b0a612fc8
MuriloFray <murilofray01@gmail.com>
2022-09-10 16:58:21 -0300

    Lista Estatica Simples Terminada

--- Estrutura De Dados 1/Lista/Lista Simples.c
+++ Estrutura De Dados 1/Lista/Lista Simples.c
@@ -4,7 +4,7 @@ typedef struct sLista
 {
     int n;
     int vet[TAM];
-}LISTA;
+} LISTA;
 
 void inicializar(LISTA *lista)
 {
@@ -25,6 +25,22 @@ int cheia(LISTA *lista)
     return 0;
 }
 
+void inserirInicio(LISTA *lista,int x)
+{
+    if(!cheia(lista))
+    {
+        int i;
+        for(i=lista->n+1; i>0; i--)
+        {
+            lista->vet[i] = lista->vet[i-1];
+        }
+        lista->n++;
+        lista->vet[0] = x;
+    }
+    else
+        printf("ERRO - LISTA CHEIA\n");
+}
+
 void inserirFinal(LISTA *lista, int x)
 {
     if(!cheia(lista))
@@ -34,6 +50,43 @@ void inserirFinal(LISTA *lista, int x)
     }
     else
         printf("ERRO - LISTA CHEIA\n");
+}
+
+
+void inserirNaPosicao(LISTA* lista, int x, int posicao)
+{
+    if(!cheia(lista))
+    {
+        if(posicao <= lista->n+1 && posicao >= 0)
+        {
+            int i;
+            for(i = lista->n+1 ; i > posicao ; i--)
+            {
+                lista->vet[i] = lista->vet[i-1];
+            }
+            lista->n++;
+            lista->vet[posicao] = x;
+        }
+        else
+            printf("ERRO - POSICAO INVALIDA\n");
+    }
+    else
+        printf("ERRO - LISTA CHEIA\n");
+}
+
+void removerInicio (LISTA *lista)
+{
+    if(!vazia(lista))
+    {
+        int i;
+        for(i=0; i<=lista->n; i++)
+        {
+            lista->vet[i] = lista->vet[i+1];
+        }
+        lista->n--;
+    }
+    else
+        printf("ERRO - LISTA VAZIA\n");
 }
 
 void removerFinal(LISTA *lista)
@@ -46,19 +99,86 @@ void removerFinal(LISTA *lista)
         printf("ERRO - LISTA VAZIA\n");
 }
 
+void removerNaPosicao (LISTA *lista, int posicao)
+{
+    if(!vazia(lista))
+    {
+        int i;
+        if(posicao <= lista->n && posicao >= 0)
+        {
+            for(i = posicao; i <= lista->n ; i++)
+            {
+                lista->vet[i] = lista->vet[i+1];
+            }
+            lista->n--;
+        }
+        else
+            printf("ERRO - POSICAO INVALIDA\n");
+    }
+    else
+        printf("ERRO - LISTA VAZIA\n");
+}
+
+void acessarPosicao(LISTA lista, int posicao)
+{
+    int i;
+    if(!vazia(&lista))
+    {
+        if(posicao >= 0 && posicao <= lista.n)
+        {
+            printf("|%d|", lista.vet[posicao]);
+        }
+        printf("ERRO - POSICAO INVALIDA\n");
+    }
+    else
+        printf("ERRO - LISTA VAZIA");
+}
+
+int procurarElemento(LISTA lista, int elemento)
+{
+    int i=0, aux=-1;
+    if(!vazia(&lista))
+    {
+        while(i <= lista.n)
+        {
+            if(lista.vet[i] == elemento)
+            {
+                aux = i;
+                i = lista.n+1;
+            }
+            i++;
+        }
+    }
+    else
+        printf("ERRO - LISTA VAZIA\n");
+    return aux;
+
+}
+
 void exibir(LISTA lista)
 {
     if(!vazia(&lista))
     {
-        for(int i=0;i<=lista.n;i++)
+        int i;
+        for(i=0; i<=lista.n; i++)
             printf("|%d|",lista.vet[i]);
+        printf("\n");
     }
 }
 void main()
 {
     LISTA lista;
     inicializar(&lista);
-    inserirFinal(&lista,5);
+    inserirInicio(&lista,3);
+    inserirInicio(&lista,2);
+    inserirInicio(&lista,1);
+    inserirNaPosicao(&lista,4,2);
     exibir(lista);
+    inserirNaPosicao(&lista,6,4);
+    exibir(lista);
+    //inserirInicio(&lista,2);
+    removerInicio(&lista);
+    exibir(lista);
+    printf("%d",procurarElemento(lista,4));
 
 }
